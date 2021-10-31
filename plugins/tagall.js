bf7a3e4e42dcf3a0423bed44b2cc8221e60d8e8b
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-10-31 12:47:42 +0700

    update tagall.js

--- plugins/tagall.js
+++ plugins/tagall.js
@@ -2,6 +2,7 @@ let handler = async (m, { conn, text, participants }) => {
   let users = participants.map(u => u.jid)
   conn.reply(m.chat, '*「 TAG ALL 」*\n\n❖ ' + users.map(v => '@' + v.replace(/@.+/, '')).join`\n❖ `, m, {
     contextInfo: { mentionedJid: users }
+})
 }
 handler.command = ['tagall']
 handler.register = true
