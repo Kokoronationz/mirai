a6d7b6f4bff2e44c520013c01d6313ec531594d8
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-10-31 17:20:01 +0700

    update how.js

--- plugins/how.js
+++ plugins/how.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, command, text }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let cok = conn.getName(who)
   conn.reply(m.chat, `
-${command} *${cok}*
+${command.charAt(0).toUpperCase() + command.slice(1)} *${cok}*
 
 *${cok}* is *${Math.floor(Math.random() * 101)}*% ${command.replace('cek', '').toUpperCase()}
 `.trim(), m, m.mentionedJid ? {
