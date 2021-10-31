537f78287cddc68fd1cd06ef8d1d40c4a95ebe62
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-10-31 11:56:20 +0700

    Main

---
+++ plugins/register.js
@@ -0,0 +1,52 @@
+const { createHash } = require('crypto')
+let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
+let handler = async function (m, { text, usedPrefix }) {
+  let rtotalreg = Object.values(global.DATABASE._data.users).filter(user => user.registered == true).length
+  let user = global.db.data.users[m.sender]
+  if (user.registered === true) throw `Anda sudah terdaftar\n\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (!Reg.test(text)) throw `Format salah\n\n*${usedPrefix}daftar nama|umur*`
+  let [_, name, splitter, age] = text.match(Reg)
+  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
+  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  age = parseInt(age)
+  if (age > 60) throw 'Maaf umur Anda terlalu tua'
+  if (age < 17) throw 'Maaf Anda belum bisa mendaftar'
+  user.name = name.trim()
+  user.age = age
+  user.regTime = + new Date
+  user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  let caption = `
+┏ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┇       *「 INFORMATION 」*
+┣ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
+┃
+┃ *Nama:* ${name}
+┃ *Umur:* ${age} tahun
+┃ *Gift:* Rp10000
+┃ *Registered:* ${rtotalreg}
+┃ *Serial Number:* 
+┃ ${sn}
+┗ ┅ ━━━━━━━━━━━━━━━━━━━━━ ┅ ━
+
+ _Simpan Serial Number anda!_
+`.trim()
+await conn.fakeReply(m.chat, caption,/* { 
+  key: { 
+    remoteJid: 'status@broadcast',
+    participant:*/ '0@s.whatsapp.net', 
+    /*fromMe: false 
+  }, message: { 
+    "imageMessage": { 
+      "mimetype": "image/jpeg", 
+      "caption": */`Registration Successful!`, /*
+      "jpegThumbnail": tnbot} } }, { contextInfo: { mentionedJid: [m.sender] } }m*/'status@broadcast')
+global.DATABASE._data.users[m.sender].uang += 10000
+}
+//handler.help = ['daftar', 'reg', 'register'].map(v => v + ' <nama>.<umur>')
+//handler.tags = ['exp']
+
+handler.command = /^(daftar|reg(ister)?)$/i
+
+module.exports = handler
+
