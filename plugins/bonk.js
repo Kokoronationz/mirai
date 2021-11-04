cc701a962ffbf58ea408a71e1a1c3da9c957b29b
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-11-04 19:33:02 +0700

    update bonk.js

--- plugins/bonk.js
+++ plugins/bonk.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, text }) => {
     let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.quoted.sender ? m.quoted.sender : m.sender
     let bonk = global.API('https://hardianto.xyz', '/api/bonk', {
     by: await conn.getProfilePicture(m.sender).catch(_ => 'https://telegra.ph/file/7995e73e508ee011722b0.png'),
-    for: await conn.getProfilePicture(who).catch(_ => ''),
+    for: await conn.getProfilePicture(who).catch(_ => 'https://telegra.ph/file/7995e73e508ee011722b0.png'),
     apikey: 'hardianto'})
     let stiker = await sticker(null, bonk, 'Bonk', global.author)
   conn.sendMessage(m.chat, stiker, MessageType.sticker, {
