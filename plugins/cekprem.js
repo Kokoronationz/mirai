3026a0dec832be0c7efa839a909f217a0f8d54c6
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-11-06 16:24:42 +0700

    update cekprem.js

--- plugins/cekprem.js
+++ plugins/cekprem.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix }) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let users = global.db.data.users[who].premdate
-  let sisa = clockString (users - new Date)
+  let sisa = msToDate (users - new Date()*1)
   conn.send2Button(m.chat, `_Dear ${await conn.getName(m.sender)}, Masa aktif premium kamu tersisa ${sisa}_`, 'Silahkan perpanjang ke Owner', 'Perpanjang', '.premium', 'Owner', '.owner')
 }
 //handler.help = ['cekprem']
@@ -11,9 +11,15 @@ handler.register = true
 
 module.exports = handler
 
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+function msToDate(ms) {
+    temp = ms
+    days = Math.floor(ms / (24 * 60 * 60 * 1000));
+    daysms = ms % (24 * 60 * 60 * 1000);
+    hours = Math.floor((daysms) / (60 * 60 * 1000));
+    hoursms = ms % (60 * 60 * 1000);
+    minutes = Math.floor((hoursms) / (60 * 1000));
+    minutesms = ms % (60 * 1000);
+    sec = Math.floor((minutesms) / (1000));
+    return days + " hari " + hours + " jam " + minutes + " menit";
+    // +minutes+":"+sec;
 }
