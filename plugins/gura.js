4913e464c058f1331e95458dd5367ea524434027
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-11-06 12:00:25 +0700

    update gura.js

--- plugins/gura.js
+++ plugins/gura.js
@@ -1,6 +1,8 @@
 const { MessageType } = require('@adiwajshing/baileys')
+let num = /([0-9])$/i
 let handler = async (m, { conn, text }) => {
 if (text.length > 7) throw 'Max. 7 huruf!'
+if (num.test(text)) throw 'Hanya Huruf!'
 if (!text) text = await conn.getName(m.sender)
 await m.reply(global.wait)
   let apiUrl = global.API( 'http://hardianto-chan.herokuapp.com', '/api/bot/gura', {
