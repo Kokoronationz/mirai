4f09dbb7b4337d3a3882331f1a61811c787cf6af
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-11-06 10:42:38 +0700

    update botchat.js

--- plugins/botchat.js
+++ plugins/botchat.js
@@ -1,10 +1,10 @@
-let handler  = async (m, { conn, text }) => {
+let handler  = async (m, { conn }) => {
   
 let name = conn.getName(m.sender)
 let pesan = `_Hai ${name}, saya disini:D_\n\n_Ada yang bisa saya bantu?_`
 conn.fakeReply(m.chat, pesan, '0@s.whatsapp.net', 'Salam Hangat Dari Owner', 'status@broadcast')
 }
-handler.customPrefix = /^Bot/i.test(m.text)
+handler.customPrefix = /Bot/
 handler.command = new RegExp
 
 module.exports = handler
