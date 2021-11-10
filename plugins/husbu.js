04e6221bf9991667b589c3ea727d9b28b039f706
Jauhariq <92896736+Jauhariq@users.noreply.github.com>
2021-11-10 08:01:18 +0700

    Biar ga down teros

--- plugins/husbu.js
+++ plugins/husbu.js
@@ -1,18 +1,21 @@
-const fetch = require('node-fetch')
-
-let handler = async (m, { conn }) => {
-    try {
-        let res = await fetch(global.API('xteam', '/randomimage/husbu2', {}, 'APIKEY'))
-        if (res.status != 200) throw await res.text()
-        let img = await res.buffer()
-        conn.sendFile(m.chat, img, '', '*Nih*', m, false, { thumbnail: Buffer.alloc(0) })
-    } catch (e) {
-        throw `Limit apikey habis atau error!`
-    }
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+let items = ["husbu anime", "anime husbu", "cowo anime", "anime ganteng"];
+let husbu = items[Math.floor(Math.random() * items.length)];
+  let res = await fetch(global.API('zeks', '/api/pinimg', {
+    q: husbu
+  }, 'apikey'))
+  if (!res.ok) throw await `${res.status} ${res.statusText}`
+  let json = await res.json()
+  if (!json.status) throw json
+  let pint = json.data[Math.floor(Math.random() * json.data.length)];
+  conn.sendFile(m.chat, pint, '', `
+*Nih pedo!*
+${text}
+`.trim(), m)
 }
 //handler.help = ['husbu']
 //handler.tags = ['weebs']
 handler.command = /^(husbu)$/i
 handler.limit = true
-
 module.exports = handler
