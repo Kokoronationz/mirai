9692b107f89a295eb299466ca57208b47f8b9cf6
ココロナシ <78072346+Kokoronationz@users.noreply.github.com>
2021-11-12 11:09:37 +0700

    update peh.js

--- plugins/peh.js
+++ plugins/peh.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-conn.send2Button(m.chat, 'Bisa ga sih kalo chat itu jangan ```P P P```', 'Utamakan Salam (Hai, Hallo, dll.)', 'Menu', '.menu', 'Owner', '.owner')
+conn.send2Button(m.chat, 'Bisa ga sih kalo chat itu jangan  ```P P P```', 'Utamakan Salam (Hai, Hallo, dll.)', 'Menu', '.menu', 'Donasi', '.donasi')
 }
 handler.customPrefix = /^Pp?p?$/g
 handler.command = new RegExp
